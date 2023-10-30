62dbc611f1645fb893595b6cccabf6db13b9e9f1
P3T3RALTF4 <148472662+P3T3RALTF4@users.noreply.github.com>
2023-10-30 11:29:09 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+let mybutton = document.getElementById("myBtn1");
+let mybutton = document.getElementById("myBtn2");
+
+function topFunction1() {
+    document.documentElement.scrollTop = 20;
+}
+function topFunction2() {
+    document.documentElement.scrollTop = 40;
+}
