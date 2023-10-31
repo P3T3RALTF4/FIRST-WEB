86f493a3c697648cc58dd513db708064cf68982d
P3T3RALTF4 <148472662+P3T3RALTF4@users.noreply.github.com>
2023-10-31 15:54:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-let mybutton = document.getElementById("myBtn1");
-let mybutton = document.getElementById("myBtn2");
-
+let mybutton1 = document.getElementById("myBtn1");
 function topFunction1() {
-    document.documentElement.scrollTop = 20;
+    document.documentElement.scrollTop = 165;
 }
+let mybutton2 = document.getElementById("myBtn2");
 function topFunction2() {
-    document.documentElement.scrollTop = 40;
+    document.documentElement.scrollTop = 750;
+}
+let mybutton3 = document.getElementById("myBtn3");
+function topFunction3() {
+    document.documentElement.scrollTop = 1340;
+}
+let mybutton4 = document.getElementById("myBtn4") 
+function topFunction4() {
+    document.documentElement.scrollTop = 2000;
+}
+let Backbutton1 = document.getElementById("backbtn1")
+function GoBackFunction1() {
+    document.documentElement.scrollTop = -165;
+}
+let Backbutton2 = document.getElementById("backbtn2")
+function GoBackFunction2(){
+    document.documentElement.scrollTop = -750;
+}
+let Backbutton3 = document.getElementById("backbtn3")
+function GoBackFunction3() {
+    document.documentElement.scrollTop = -1560;
+}
+let Backbutton4 = document.getElementById("backbtn4")
+function GoBackFunction4() {
+    document.documentElement.scrollTop = -1560;
 }
