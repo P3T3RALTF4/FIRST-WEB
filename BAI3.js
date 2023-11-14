6dbc07b9d86de695a17ad00747398be5a038b527
P3T3RALTF4 <148472662+P3T3RALTF4@users.noreply.github.com>
2023-11-14 22:07:16 +0700

    Add files via upload

---
+++ BAI3.js
@@ -0,0 +1,12 @@
+let Mybutton1 = document.getElementById("MyBtn1")
+function TopFunction1() {
+    document.documentElement.scrollTop = 0;
+}
+let Mybutton2 = document.getElementById("MyBtn2")
+function TopFunction2() {
+    document.documentElement.scrollTop = 500;
+}
+let Mybutton3 = document.getElementById("MyBtn3")
+function TopFunction3() {
+    document.documentElement.scrollTop = 1300;
+}
